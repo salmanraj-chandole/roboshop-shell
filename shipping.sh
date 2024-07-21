23811f2ff9e5f4072a21184c2b8d625c02ae118d
Salman Raj Chandole <salmanrajchandole@gmail.com>
2024-07-21 14:00:50 +0530

    Shipping Setup

--- shipping.sh
+++ shipping.sh
@@ -9,11 +9,12 @@ unzip /tmp/shipping.zip
 cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
+
 dnf install mysql -y
 mysql -h mysql.dev.salmanrajb80.online -uroot -pRoboShop@1 < /app/db/schema.sql
 mysql -h mysql.dev.salmanrajb80.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 mysql -h mysql.dev.salmanrajb80.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+
+systemctl daemon-reload
+systemctl enable shipping
 systemctl restart shipping
